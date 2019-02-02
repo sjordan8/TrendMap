e5172141c969d3bf7262dc6ff896b6349c259a4c
[Sean Jordan] <[sean.j7767@gmail.com]>
2019-02-02 13:53:52 -0800

    Added tweepy, updated gitignore, UI changes

--- TrendMap/main/views.py
+++ TrendMap/main/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render, redirect
 
+import tweepy
+
+
 # Create your views here.
 
 def index(request):
     context= {}
+    return render(request, 'index.html', context)
+
+
+def get_tweets(request):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    public_tweets = api.home_timeline()
+    for tweet in public_tweets:
+        print(tweet.text)
+    context= {
+        'public_tweets': public_tweets,
+    }
     return render(request, 'index.html', context)
